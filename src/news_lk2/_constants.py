a478fe5e7a1b360f3f00b6dd6dde703fc5a3fa1d
Ranuka <140217957+RanukaDinsitha@users.noreply.github.com>
2024-08-13 19:52:43 +1200

    Update _constants.py

--- src/news_lk2/_constants.py
+++ src/news_lk2/_constants.py
@@ -1,11 +1,11 @@
 """Constants."""
 
-CACHE_NAME = 'news_lk2'
+CACHE_NAME = 'Newsfetch'
 CACHE_TIMEOUT = 3600
 TEST_MODE = False
 
 WORDS_PER_MINUTE = 250
 
-URL_GITHUB_BASE = 'https://github.com/nuuuwan/news_lk2/blob/data'
-DIR_TMP_BASE = '/tmp/news_lk2'
+URL_GITHUB_BASE = 'https://github.com/rangana/Newsfetch/blob/data'
+DIR_TMP_BASE = '/tmp/Newsfetch'
 DELIM_MD = '\n' * 2
